6f6f357e4e4fc21b2626d8e232702f89c272f74a
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-11-09 20:55:48 +0100

    Improve test configuration and naming

--- tests/calculations.cc
+++ tests/calculations.cc
@@ -4,7 +4,7 @@ extern "C" {
 
 #include <gtest/gtest.h>
 
-TEST(HelloWorld, BasicAddition) {
+TEST(HelloWorld, BasicAdditionTest) {
   EXPECT_EQ(AddNumbers(1, 1), 2);
   EXPECT_EQ(AddNumbers(2, 2), 4);
   EXPECT_EQ(AddNumbers(5, 5), 10);
