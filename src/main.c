7505a36dd4e839efac91979dd8cf1937e57406d7
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-11-09 20:55:25 +0100

    Add sample usage og libcurl

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
+#include "main.h"
+
+#include <curl/curl.h>
+#include <curl/easy.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "calculations.h"
+#include "string_builder.h"
 
 int main() {
+  /*
+   * Basic output test
+   */
   const int kFirstNumber = 3;
   const int kSecondNumber = 5;
 
@@ -12,6 +21,32 @@ int main() {
   printf("First added_numbers: %d\n", kFirstNumber);
   printf("Second added_numbers: %d\n", kSecondNumber);
   printf("Added numbers: %d\n", added_numbers);
+
+  /*
+   * Some string test
+   */
+  char *output_string = malloc(kBufferSize * sizeof(char));
+  BuildString(output_string, "The number is: ", kSecondNumber);
+  printf("%s", output_string);
+  free(output_string);
+
+  /*
+   * Some cURL
+   */
+  CURL *curl = curl_easy_init();
+
+  if (!curl) {
+    return EXIT_FAILURE;
+  }
+
+  char endpoint[kBufferSize] = "";
+  strcat(endpoint, kRejseplanenApiBaseUrl);
+  strcat(endpoint, "stopsNearby?coordX=55673059&coordY=12565557&format=json");
+
+  curl_easy_setopt(curl, CURLOPT_URL, endpoint);
+
+  curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
 
   return EXIT_SUCCESS;
 }
---
+++ src/main.h
@@ -0,0 +1,9 @@
+#ifndef AAU_P1_SOFTWARE_SRC_MAIN_H_
+#define AAU_P1_SOFTWARE_SRC_MAIN_H_
+
+const char* kRejseplanenApiBaseUrl =
+    "https://xmlopen.rejseplanen.dk/bin/rest.exe/";
+
+enum Constants { kBufferSize = 256 };
+
+#endif  // AAU_P1_SOFTWARE_SRC_MAIN_H_
