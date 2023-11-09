a088da746f29c0833bc8671acf6146974a0514ba
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-11-09 20:58:48 +0100

    Fix header include placement in string_builder.c

--- src/string_builder.c
+++ src/string_builder.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-
 #include "string_builder.h"
+
+#include <stdio.h>
 
 void BuildString(char* output_string, const char* base_string, int number) {
   sprintf(output_string, "%s%d", base_string, number);
