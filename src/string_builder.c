ec295a491fc8925690e3a3ff4b089370b2049f54
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-11-11 18:14:01 +0100

    Add NOLINTNEXTLINE to string_builder.c

--- src/string_builder.c
+++ src/string_builder.c
@@ -3,5 +3,6 @@
 #include <stdio.h>
 
 void BuildString(char* output_string, const char* base_string, int number) {
+  // NOLINTNEXTLINE
   snprintf(output_string, 60, "%s%d", base_string, number);
 }
