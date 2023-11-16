3b585fbe4b8108efde4de2c19f154ae2cdf2af7d
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-11-16 09:41:28 +0100

    Update ClangTidy configuration, update sample code

--- src/application.h
+++ src/application.h
@@ -1,11 +1,20 @@
 #ifndef AAU_P1_SOFTWARE_SRC_APPLICATION_H_
 #define AAU_P1_SOFTWARE_SRC_APPLICATION_H_
 
+#ifdef __APPLE__
+#include <sys/_types/_size_t.h>
+#endif
+
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 const char* kRejseplanenApiBaseUrl =
     "https://xmlopen.rejseplanen.dk/bin/rest.exe/";
 
 enum Constants { kBufferSize = 256 };
+
+typedef struct Response {
+  char* body;
+  size_t size;
+} Response;
 
 void Run();
 
