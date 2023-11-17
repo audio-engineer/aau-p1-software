43690ead9263b6ba85c5f075c3916055f1d6a8c5
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-11-17 16:34:19 +0100

    Move Response struct to api_handler.h

--- src/api_handler.h
+++ src/api_handler.h
@@ -1,8 +1,18 @@
 #ifndef AAU_P1_SOFTWARE_SRC_API_HANDLER_H_
 #define AAU_P1_SOFTWARE_SRC_API_HANDLER_H_
 
+#ifdef __linux__
+#include <stdlib.h>
+#elifdef __APPLE__
+#include <sys/_types/_size_t.h>
+#endif
+
 #include "curl/curl.h"
-#include "globals.h"
+
+typedef struct Response {
+  char* body;
+  size_t size;
+} Response;
 
 void DoRequest(CURL* curl, const char* endpoint, Response* response);
 
