9020a2de7b989eef77bd20d384fe2dc0284fe5fd
Martin Kedmenec <70742985+audio-engineer@users.noreply.github.com>
2023-11-23 16:38:07 +0100

    Start work on support for user preferences (#11)
    * Start work on support for user preferences
    
    * Attempt to fix ClangTidy issues
    
    * Change name of preferences file parameters

---
+++ src/preferences.c
@@ -0,0 +1,89 @@
+#include "preferences.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "cJSON.h"
+
+void InitializePreferenceFile(FILE* preferences) {
+  const double kValue = 0.25;
+  char* serialized_json = NULL;
+
+  cJSON* json_preferences = cJSON_CreateObject();
+
+  cJSON* price_value = cJSON_CreateNumber(kValue);
+  cJSON_AddItemToObject(json_preferences, "price", price_value);
+
+  cJSON* time_value = cJSON_CreateNumber(kValue);
+  cJSON_AddItemToObject(json_preferences, "time", time_value);
+
+  cJSON* environment_value = cJSON_CreateNumber(kValue);
+  cJSON_AddItemToObject(json_preferences, "environment", environment_value);
+
+  cJSON* health_value = cJSON_CreateNumber(kValue);
+  cJSON_AddItemToObject(json_preferences, "health", health_value);
+
+  serialized_json = cJSON_Print(json_preferences);
+
+  fputs(serialized_json, preferences);
+  fseek(preferences, 0, SEEK_SET);
+}
+
+FILE* GetPreferenceFile() {
+  FILE* preferences = NULL;
+
+  if (access("preferences.json", F_OK) != 0) {
+    preferences = fopen("preferences.json", "w+");
+
+    if (!preferences) {
+      perror("File creation failed");
+
+      return NULL;
+    }
+
+    InitializePreferenceFile(preferences);
+
+    return preferences;
+  }
+
+  preferences = fopen("preferences.json", "r+");
+
+  if (!preferences) {
+    perror("File opening failed");
+
+    return NULL;
+  }
+
+  return preferences;
+}
+
+void SetUserPreference(FILE* preferences, const char* key, double value) {
+  fseek(preferences, 0, SEEK_END);
+  unsigned long file_size = (unsigned long)ftell(preferences);
+  fseek(preferences, 0, SEEK_SET);
+
+  char* file_buffer = calloc(file_size, sizeof(char));
+
+  size_t bytes = fread(file_buffer, sizeof(char), file_size, preferences);
+  fseek(preferences, 0, SEEK_SET);
+
+  if (bytes < file_size) {
+    perror("Something happened while reading user preference");
+
+    return;
+  }
+
+  file_buffer[file_size] = '\0';
+
+  cJSON* read_file = cJSON_Parse(file_buffer);
+  free(file_buffer);
+  cJSON* read_value = cJSON_GetObjectItem(read_file, key);
+
+  read_value->valuedouble = value;
+
+  char* serialized_json = cJSON_Print(read_file);
+  serialized_json[file_size - 1] = '\n';
+
+  fputs(serialized_json, preferences);
+  fseek(preferences, 0, SEEK_SET);
+}
---
+++ src/preferences.h
@@ -0,0 +1,12 @@
+#ifndef AAU_P1_SOFTWARE_SRC_PREFERENCES_H_
+#define AAU_P1_SOFTWARE_SRC_PREFERENCES_H_
+
+#include <stdio.h>
+
+FILE* GetPreferenceFile();
+
+void SetUserPreference(FILE* preferences, const char* key, double value);
+
+double GetUserPreference(FILE* preferences, const char* key);
+
+#endif  // AAU_P1_SOFTWARE_SRC_PREFERENCES_H_
