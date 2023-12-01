c56d05a2c09750185944929eba67493154ce435a
Kristiyan Georgiev <2003kristiyan@gmail.com>
2023-12-01 13:01:56 +0100

    experiment with tests

--- tests/calculations.cc
+++ tests/calculations.cc
@@ -4,6 +4,8 @@ extern "C" {
 
 #include <gtest/gtest.h>
 
-TEST(CalculationsTest, CalculatePrice) {
-  EXPECT_EQ(988, CalculatePrice('c', 30, false, 17));
-}
+const CalculatePriceParameters kCalculatePriceParameters = {'c', 30, false, 17};
+const auto kCalculatePriceParametersAddress = &kCalculatePriceParameters;
+const int kCalculatedPrice = CalculatePrice(kCalculatePriceParametersAddress);
+
+TEST(CalculationsTest, CalculatePrice) { EXPECT_EQ(988, kCalculatedPrice); }
