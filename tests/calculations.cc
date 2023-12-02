4405512fe37ebae9c908822a0c3edbe184fd5197
Kristiyan Georgiev <2003kristiyan@gmail.com>
2023-12-03 00:47:57 +0100

    update calculations also I need sleep

--- tests/calculations.cc
+++ tests/calculations.cc
@@ -4,30 +4,52 @@ extern "C" {
 
 #include <gtest/gtest.h>
 
-TEST(CalculationsTest, CalculatePrice) {
-  const CalculatePriceParameters kParameters01 = {'t', 30, false, 0};
-  const CalculatePriceParameters kParameters02 = {'t', 100, false, 10};
-  const CalculatePriceParameters kParameters03 = {'t', 150, true, 0};
-  const CalculatePriceParameters kParameters04 = {'t', 400, true, 0};
+TEST(CalculationsTest, CalculatePrice01) {
+  const CalculatePriceParameters kParameters = {'t', 30, false, 0};
+  EXPECT_EQ(900, CalculatePrice(&kParameters));
+}
 
-  const CalculatePriceParameters kParameters05 = {'c', 30, false, 17};
-  const CalculatePriceParameters kParameters06 = {'c', 100, false, 0};
-  const CalculatePriceParameters kParameters07 = {'c', 200, true, 33};
-  const CalculatePriceParameters kParameters08 = {'e', 50, false, 10};
+TEST(CalculationsTest, CalculatePrice02) {
+  const CalculatePriceParameters kParameters = {'t', 100, false, 10};
+  EXPECT_EQ(2730, CalculatePrice(&kParameters));
+}
 
-  const CalculatePriceParameters kParameters09 = {'b', 320, true, 15};
-  const CalculatePriceParameters kParameters10 = {'w', 40, false, 0};
+TEST(CalculationsTest, CalculatePrice03) {
+  const CalculatePriceParameters kParameters = {'t', 150, true, 0};
+  EXPECT_EQ(1294, CalculatePrice(&kParameters));
+}
 
-  EXPECT_EQ(900, CalculatePrice(&kParameters01));
-  EXPECT_EQ(2730, CalculatePrice(&kParameters02));
-  EXPECT_EQ(1294, CalculatePrice(&kParameters03));  // same as in our report
-  EXPECT_EQ(1714, CalculatePrice(&kParameters04));
+TEST(CalculationsTest, CalculatePrice04) {
+  const CalculatePriceParameters kParameters = {'t', 400, true, 0};
+  EXPECT_EQ(1714, CalculatePrice(&kParameters));
+}
 
-  EXPECT_EQ(1838, CalculatePrice(&kParameters05));  // same as in our report
-  EXPECT_EQ(6450, CalculatePrice(&kParameters06));
-  EXPECT_EQ(4243, CalculatePrice(&kParameters07));
-  EXPECT_EQ(1683, CalculatePrice(&kParameters08));
+TEST(CalculationsTest, CalculatePrice05) {
+  const CalculatePriceParameters kParameters = {'c', 30, false, 17};
+  EXPECT_EQ(1838, CalculatePrice(&kParameters));
+}
 
-  EXPECT_EQ(100, CalculatePrice(&kParameters09));
-  EXPECT_EQ(0, CalculatePrice(&kParameters10));
+TEST(CalculationsTest, CalculatePrice06) {
+  const CalculatePriceParameters kParameters = {'c', 100, false, 0};
+  EXPECT_EQ(6450, CalculatePrice(&kParameters));
+}
+
+TEST(CalculationsTest, CalculatePrice07) {
+  const CalculatePriceParameters kParameters = {'c', 200, true, 33};
+  EXPECT_EQ(4243, CalculatePrice(&kParameters));
+}
+
+TEST(CalculationsTest, CalculatePrice08) {
+  const CalculatePriceParameters kParameters = {'e', 50, false, 10};
+  EXPECT_EQ(1683, CalculatePrice(&kParameters));
+}
+
+TEST(CalculationsTest, CalculatePrice09) {
+  const CalculatePriceParameters kParameters = {'b', 320, true, 15};
+  EXPECT_EQ(100, CalculatePrice(&kParameters));
+}
+
+TEST(CalculationsTest, CalculatePrice10) {
+  const CalculatePriceParameters kParameters = {'w', 40, false, 0};
+  EXPECT_EQ(0, CalculatePrice(&kParameters));
 }
