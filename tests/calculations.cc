a70d35dc08b991b8815366ce97ab2b3b89f3fa2e
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-12-03 13:12:35 +0100

    Slightly improve tests, add CLion test configurations

--- tests/calculations.cc
+++ tests/calculations.cc
@@ -4,52 +4,52 @@ extern "C" {
 
 #include <gtest/gtest.h>
 
-TEST(CalculationsTest, CalculatePrice01) {
-  const CalculatePriceParameters kParameters = {'t', 30, false, 0};
+TEST(CalculatePriceTest, Train30kmAdult0) {
+  const CalculatePriceParameters kParameters = {kTrain, 30, false, 0};
   EXPECT_EQ(900, CalculatePrice(&kParameters));
 }
 
-TEST(CalculationsTest, CalculatePrice02) {
-  const CalculatePriceParameters kParameters = {'t', 100, false, 10};
+TEST(CalculatePriceTest, Train100kmAdult10) {
+  const CalculatePriceParameters kParameters = {kTrain, 100, false, 10};
   EXPECT_EQ(2730, CalculatePrice(&kParameters));
 }
 
-TEST(CalculationsTest, CalculatePrice03) {
-  const CalculatePriceParameters kParameters = {'t', 150, true, 0};
+TEST(CalculatePriceTest, Train150kmYoung0) {
+  const CalculatePriceParameters kParameters = {kTrain, 150, true, 0};
   EXPECT_EQ(1294, CalculatePrice(&kParameters));
 }
 
-TEST(CalculationsTest, CalculatePrice04) {
-  const CalculatePriceParameters kParameters = {'t', 400, true, 0};
+TEST(CalculatePriceTest, Train400kmYoung0) {
+  const CalculatePriceParameters kParameters = {kTrain, 400, true, 0};
   EXPECT_EQ(1714, CalculatePrice(&kParameters));
 }
 
-TEST(CalculationsTest, CalculatePrice05) {
-  const CalculatePriceParameters kParameters = {'c', 30, false, 17};
+TEST(CalculatePriceTest, Car30kmAdult17) {
+  const CalculatePriceParameters kParameters = {kCar, 30, false, 17};
   EXPECT_EQ(1838, CalculatePrice(&kParameters));
 }
 
-TEST(CalculationsTest, CalculatePrice06) {
-  const CalculatePriceParameters kParameters = {'c', 100, false, 0};
+TEST(CalculatePriceTest, Car100kmAdult0) {
+  const CalculatePriceParameters kParameters = {kCar, 100, false, 0};
   EXPECT_EQ(6450, CalculatePrice(&kParameters));
 }
 
-TEST(CalculationsTest, CalculatePrice07) {
-  const CalculatePriceParameters kParameters = {'c', 200, true, 33};
+TEST(CalculatePriceTest, Car200kmYoung33) {
+  const CalculatePriceParameters kParameters = {kCar, 200, true, 33};
   EXPECT_EQ(4243, CalculatePrice(&kParameters));
 }
 
-TEST(CalculationsTest, CalculatePrice08) {
-  const CalculatePriceParameters kParameters = {'e', 50, false, 10};
+TEST(CalculatePriceTest, Ev50kmAdult10) {
+  const CalculatePriceParameters kParameters = {kEl, 50, false, 10};
   EXPECT_EQ(1683, CalculatePrice(&kParameters));
 }
 
-TEST(CalculationsTest, CalculatePrice09) {
-  const CalculatePriceParameters kParameters = {'b', 320, true, 15};
+TEST(CalculatePriceTest, Bike320kmYoung15) {
+  const CalculatePriceParameters kParameters = {kBike, 320, true, 15};
   EXPECT_EQ(100, CalculatePrice(&kParameters));
 }
 
-TEST(CalculationsTest, CalculatePrice10) {
-  const CalculatePriceParameters kParameters = {'w', 40, false, 0};
+TEST(CalculatePriceTest, Walk40kmAdult0) {
+  const CalculatePriceParameters kParameters = {kWalk, 40, false, 0};
   EXPECT_EQ(0, CalculatePrice(&kParameters));
 }
