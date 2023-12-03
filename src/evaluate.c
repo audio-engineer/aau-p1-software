ffb8d1fc631d4a06f1210ce0d21a17f9bd8998eb
Mads Heilmann <mads.heilmann@hotmail.com>
2023-12-03 14:27:10 +0100

    Rebased and updated evaluate.c

--- src/evaluate.c
+++ src/evaluate.c
@@ -1,16 +1,33 @@
 
 
 
-typedef struct trip{
-  double emissions;
-  double price;
-  double time;
-}trip;
+typedef struct Trip{
+  int trip_id;
+  double emission_amount;
+  double emission_score;
+  double prise_amount;
+  double price_score;
+  double time_amount;
+  double time_score;
+  double overall_score;
+} Trip;
 
+int Large(Trip* arr, size_t arr_size){
+  return 0;
+}
 
+int Small(Trip* arr, size_t arr_size){
+  return 0;
+}
 
-void evaluate(){
-  trip* trip_arr = NULL;
+void Evaluate(){
+  Trip* trip_arr = NULL;
+
+  trip_arr[0].trip_id = 23
+  size_t trip_arr_size_elements = sizeof(trip_arr) / sizeof(Trip);
+
+  Large(trip_arr, trip_arr_size_elements);
+  Small(trip_arr, trip_arr_size_elements);
 
 }
 
